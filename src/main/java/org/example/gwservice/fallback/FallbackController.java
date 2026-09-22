d67c5f5bc5b8cfed11948ccb84c800c0f70767b9
Hoàng Đức Mạnh <hoangducmanh2004789@gmail.com>
2026-09-22 16:21:02 +0700

    fix fallback

--- src/main/java/org/example/gwservice/fallback/FallbackController.java
+++ src/main/java/org/example/gwservice/fallback/FallbackController.java
@@ -2,7 +2,6 @@ package org.example.gwservice.fallback;
 
 import org.example.javaframework.web.api.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/auth")
+    @RequestMapping("/auth")
     public Response<String> handleAuthFallback() {
         return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"Auth service is currently unavailable");
     }
 
-    @GetMapping("/user")
+    @RequestMapping("/user")
     public Response<String> handleUserFallback() {
         return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"User service is currently unavailable");
     }
 
-    @GetMapping("/lab")
+    @RequestMapping("/lab")
     public Response<String> handleLabFallback() {
         return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"Lab service is currently unavailable");
     }
