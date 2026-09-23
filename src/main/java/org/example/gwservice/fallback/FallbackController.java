35fcb4e7e15f577a75c806672f27533265e04919
Hoàng Đức Mạnh <hoangducmanh2004789@gmail.com>
2026-09-23 15:33:25 +0700

    fix: response fall back

--- src/main/java/org/example/gwservice/fallback/FallbackController.java
+++ src/main/java/org/example/gwservice/fallback/FallbackController.java
@@ -2,6 +2,7 @@ package org.example.gwservice.fallback;
 
 import org.example.javaframework.web.api.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class FallbackController {
 
     @RequestMapping("/auth")
-    public Response<String> handleAuthFallback() {
-        return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"Auth service is currently unavailable");
+    public ResponseEntity<Response<String>> handleAuthFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(), "Auth service is currently unavailable"));
     }
 
     @RequestMapping("/user")
-    public Response<String> handleUserFallback() {
-        return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"User service is currently unavailable");
+    public ResponseEntity<Response<String>> handleUserFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(), "User service is currently unavailable"));
     }
 
     @RequestMapping("/lab")
-    public Response<String> handleLabFallback() {
-        return Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(),"Lab service is currently unavailable");
+    public ResponseEntity<Response<String>> handleLabFallback() {
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Response.failure(HttpStatus.SERVICE_UNAVAILABLE.toString(), "Lab service is currently unavailable"));
     }
 }
